2ae48dc9d08d63112e5c3a1bd069d2fe99db284d
Sergey Samarsky <js.samarsky@gmail.com>
2022-12-10 13:06:06 +0300

    feat: added basic structure with html and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Categories } from "./components/categories/Categories";
+import { Header } from "./components/header/Header";
+import { PizzaBlock } from "./components/pizzaBlock/PizzaBlock";
+import { Sort } from "./components/sort/Sort";
+import "./scss/app.scss";
 
 function App() {
   return (
-    <div>
-      
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
